864994a81b891485be76627d82f1b74fb38ddaaf
Administrator <Administrator@PC-20170504PCLA>
2017-05-27 17:25:00 +0800

    1

--- Cosys/CoSys.WebService/WebService.News.cs
+++ Cosys/CoSys.WebService/WebService.News.cs
@@ -1115,14 +1115,18 @@ namespace CoSys.Service
                 //邮箱头盖
                 var plushMethod = "";
                 var typeNameDic = Cache_Get_DataDictionary()[GroupCode.Channel];
-
-                news.NewsTypeName = typeNameDic.ContainsKey(news.NewsTypeID) ? typeNameDic[news.NewsTypeID].Value : "无";
-                Cache_Get_DataDictionary()[GroupCode.Channel].Values.Where(x => (x.Key.GetLong() & channelFlag) != 0 && x.Remark.IsNotNullOrEmpty()).ToList().ForEach(x =>
+                
+                Cache_Get_DataDictionary()[GroupCode.Channel].Values.Where(x => (x.Key.GetLong() & channelFlag) != 0).ToList().ForEach(x =>
                   {
                       plushMethod += " " + x.Value;
-                      var result = WebHelper.SendMail(x.Remark, $"{CustomHelper.GetValue("Company_Email_Title")} 笔名:{news.PenName}", news.Content, news.Paths);
-                      ;
-                      var getResult = WebHelper.GetPage("http://5.weboss.hk/newsApi.php",$"title={news.Title}&catid={news.NewsTypeName}&body={news.Content}&author={news.PenName}");
+                      if (x.Remark.IsNotNullOrEmpty())
+                      {
+                          var result = WebHelper.SendMail(x.Remark, $"{CustomHelper.GetValue("Company_Email_Title")} 笔名:{news.PenName}", news.Content, news.Paths);
+                      }
+                      else
+                      {
+                          var getResult = WebHelper.GetPage("http://5.weboss.hk/newsApi.php", $"title={news.Title}&catid={x.Value}&body={news.Content}&author={news.PenName}");
+                      }
                   });
                 Add_Log(LogCode.Plush, id, Client.LoginAdmin.ID, $"发布于{plushMethod}");
                 if (db.SaveChanges() > 0)
